4ac59d62ace2624ee945fbec666a704cf72d0c5f
HarshDex <nature4917@gmail.com>
2023-09-29 06:51:30 +0530

    add : added page1 and page2

---
+++ index.js
@@ -0,0 +1,72 @@
+function revealToSpan(){
+    document.querySelectorAll(".reveal").forEach(function(elem){
+        let spanChild = document.createElement("span");
+        let spanParent = document.createElement("span");
+        spanParent.classList.add("parent");
+        spanChild.classList.add("child");
+        spanChild.innerHTML = elem.innerHTML;
+        spanParent.appendChild(spanChild);
+        elem.innerHTML = "";
+        elem.appendChild(spanParent);
+    })
+}
+revealToSpan();
+var tl = gsap.timeline();
+tl.from(".child span",{
+    x : "10vw",
+    duration : .7,
+    stagger : .3,
+    delay : .1,
+    easeOut : Expo.easeOut,
+})
+tl.to(".parent .child",{
+    y : "-100%",
+    duration : .5,
+}).to(".yellow",{
+    height : "100vh",
+    top : 0,
+    duration : .6,
+}).to(".yellow",{
+    height : "0vh",
+    top : 0,
+    duration : .4,
+    delay : .2,
+}).to(".loader-about-container",{
+    height : "0vh",
+    top : 0,
+    duration : .4,
+    delay : .2,
+})
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.to(".page2",{
+    backgroundColor : "rgb(4, 40, 41)",
+    scrollTrigger : {
+        trigger : ".page1",
+        start : "top 80%",
+        end : "top 10%",
+        markers : true,
+        scrub : 1,
+        id : "page2"
+    }
+})
+gsap.from(".page2-part1",{
+    x : -370,
+    ease : "power1.out",
+    scrollTrigger : {
+        trigger : ".page1",
+        start : "top 60%",
+        end : "top 5%",
+        scrub :2,
+    }
+})
+gsap.from(".page2-part2",{
+    x : 500,
+    ease : "power1.out",
+    scrollTrigger : {
+        trigger : ".page1",
+        start : "top 60%",
+        end : "top 15%",
+        scrub :2,
+    }
+})
